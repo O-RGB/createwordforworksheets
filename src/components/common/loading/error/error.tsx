da0590a1a839b5435a56ebbae6ce5a53e8727031
O-RGB <ok.oza5555@gmail.com>
2023-09-16 16:56:17 +0700

    error

--- src/components/common/loading/error/error.tsx
+++ src/components/common/loading/error/error.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ButtonCustom from "../../button";
+import { Result } from "antd";
 
 interface ErrorProps {
   mess: string;
@@ -15,14 +16,24 @@ const ErrorComponent: React.FC<ErrorProps> = ({
   return (
     <div className="fixed w-full h-screen bg-black/10 flex justify-center items-center p-3">
       <div className="p-5 max-w-3xl rounded-md overflow-hidden shadow-md flex flex-col gap-3 justify-center items-center bg-white">
-        <div className="p-2 rounded-md bg-red-500 text-white">Error</div>
-        <div>
-          <div>Message: {mess}</div>
-          <div className="break-all">Google Sheets URL: {userInitLocal?.googlesheets}</div>
-        </div>
-        <ButtonCustom type="primary" onClick={onClickSettingUrl}>
-          เปลี่ยนการตั้งค่าใหม่
-        </ButtonCustom>
+        <Result
+          status="error"
+          title={mess}
+          subTitle={
+            <div className="break-all">
+              Google Sheets URL: {userInitLocal?.googlesheets}
+            </div>
+          }
+          extra={[
+            <ButtonCustom
+              type="primary"
+              key="console"
+              onClick={onClickSettingUrl}
+            >
+              เปลี่ยนการตั้งค่าใหม่
+            </ButtonCustom>,
+          ]}
+        />
       </div>
     </div>
   );
--- src/components/common/loading/loading/loading.tsx
+++ src/components/common/loading/loading/loading.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { LoadingOutlined } from "@ant-design/icons";
 
 interface LoadingProps {}
 
 const LoadingComponent: React.FC<LoadingProps> = ({}) => {
   return (
     <div className="fixed w-full h-screen bg-black/10 flex justify-center items-center">
-      <div className="p-3 rounded-md overflow-hidden shadow-md flex flex-col gap-3">
-        <div>loaidng</div>
+      <div className="p-4 rounded-md overflow-hidden shadow-md flex justify-center items-center gap-3 bg-white">
+        <div>
+          <LoadingOutlined className="text-3xl" />
+        </div>
+        <div>กำลังโหลดข้อมูลจาก Google Sheets</div>
       </div>
     </div>
   );
