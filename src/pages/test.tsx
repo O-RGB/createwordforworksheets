fbc641f3b8dcc782826eba526579e6f3ca8a99cf
O-RGB <ok.oza5555@gmail.com>
2023-09-29 19:04:29 +0700

    te

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -11,13 +11,13 @@ const Test: NextPage = () => {
           className="w-full h-full"
         ></iframe>
       </div>
-      <div className="w-full h-full">
+      {/* <div className="w-full h-full">
         <div>Copy URL</div>
         <iframe
           src="https://th.kerryexpress.com/th/track/?track=NTZlMjQxNTE3MzlhZWY2NDRiMTQ4NDI1ODRhN2QyYWZmSHg4OThjMDAzOGQ1N2EzZDE4Mjk2ZjhkNzE3OWY3MGY2MDJmSHg4UlJFVDAwMDEzNDI5M00zZkh4OGQ3YTk2NjY3OGZkNjdmNzQ0NWFiM2JiOTQ1NjYzM2Y2Zkh4ODY0ZWNkY2M5MjNlNzJkZWFmYWI4MWY4MGQ4M2JjNTBj"
           className="w-full h-full"
         ></iframe>
-      </div>
+      </div> */}
     </div>
   );
 };
